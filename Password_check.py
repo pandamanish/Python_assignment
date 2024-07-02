a23375a825ea10e18ad4ae289fb0b3438280eebe
Manish Panda <pandamanish75@gmail.com>
2024-07-03 00:34:31 +0530

    added all 3assign

---
+++ Password_check.py
@@ -0,0 +1,60 @@
+import string
+def password_strength(password):
+    # Initialized flags to check different criteria
+    uppercase_check = False
+    lowercase_check = False
+    digitcase_check = False
+    specialchar_check = False
+    #list to append not match cases
+    check_unsolved = []
+    
+    # Checked minimum length requirement of password
+    if len(password) < 8:
+        check_unsolved.append("*Length is too short (Minimum 8 Characters)\n")
+    
+    # Iterating over each characters in the password to match cases
+    for i in password:
+        if i.isupper():
+            uppercase_check = True
+        if i.islower():
+            lowercase_check = True
+        if i.isdigit():
+            digitcase_check = True
+        if not i.isalnum():
+            specialchar_check = True
+    
+    # Checked if all required cases are met or not, if not append to check_unsolved
+    if not uppercase_check:
+        check_unsolved.append("*Password must be missing with atleast 1 Uppercase letter\n")
+        
+    if not lowercase_check:
+        check_unsolved.append("*Password must be missing with atleast 1 Lowercase letter\n")
+        
+    if not digitcase_check:
+        check_unsolved.append("*Password must be missing with atleast 1 Numeric digit\n")
+        
+    if not specialchar_check:
+        check_unsolved.append("*Password must be missing with atleast 1 Special character\n")
+    
+    # Checking if the 'check_unsolved' is not empty then printing the statements
+    if check_unsolved:
+        print("Password is not Strong!Kindly find the changes to be made below: \n")
+        for check in check_unsolved:
+            print(check)
+        return False
+    
+    else:
+        
+        return True
+
+
+
+
+# checking if the strength is true and if not it will ask for the password again
+while True:
+    password = input("Enter a strong password: ")
+    if password_strength(password):
+        print("Hurray!! Your Password is Strong enough!!")
+        break
+    else:
+        continue
---
+++ backup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import shutil
+import datetime
+
+
+def file_check(src_dir,target_dir):
+    try:
+        # Checking if the source and target directories exist or not:
+        if not os.path.isdir(src_dir):
+            print(f"The source directory {src_dir} is not present ")
+        if not os.path.isdir(target_dir):
+            print(f"The target directory {target_dir} does not exist")
+        
+        # Retrieving the files from the source folder
+        file_name=os.listdir(src_dir)
+        files=[item for item in file_name if os.path.isfile(os.path.join(src_dir,item))]
+        for file_name in files:
+            target_file=os.path.join(target_dir,file_name)
+
+            
+        #Checking if filename exist then append with timestamp else copy
+        if os.path.exists(target_file):
+            filename,extension=os.path.splitext(file_name)
+            timestr=datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            new_filename=f"{filename}_{timestr}{extension}"
+            new_target_dir=os.path.join(target_dir,new_filename)
+            shutil.copy(target_file,new_target_dir)
+            print(f"File already exists.Copied as {new_filename}")
+        else:
+            shutil.copy(src_dir,target_dir)
+            print( f"File copied to {target_dir}")
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+
+if len(sys.argv) !=3:
+    print("Syntax: Backup.py <source_directory> <target directory>")
+    sys.exit(2)
+
+src_dir=sys.argv[1]
+target_dir=sys.argv[2]
+file_check(src_dir,target_dir)
+    
+    
+    
